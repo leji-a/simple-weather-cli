88249ec27c6e0a2611b2e5a59d3953b36f6697e8
leji <79941501+leji-a@users.noreply.github.com>
2023-10-15 13:45:00 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+import requests
+import argparse
+import pyfiglet
+from simple_chalk import chalk
+
+#API Key for openWeatherMap
+API_KEY = '88e43364b8100d65caffc3e69a195021'
+#Base URL for openWeatherMap API
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+#Mapping of weather codes to weather icons
+WEATHER_ICONS = {
+    # day icons
+    "01d": "☀️",
+    "02d": "⛅️",
+    "03d": "☁️",
+    "04d": "☁️",
+    "09d": "🌧",
+    "10d": "🌦",
+    "11d": "⛈",
+    "13d": "🌨",
+    "50d": "🌫",
+    # night icons
+    "01n": "🌙",
+    "02n": "☁️",
+    "03n": "☁️",
+    "04n": "☁️",
+    "09n": "🌧",
+    "10n": "🌦",
+    "11n": "⛈",
+    "13n": "🌨",
+    "50n": "🌫",
+}
+parser = argparse.ArgumentParser(description="Check the weather for a certain country/city.")
+parser.add_argument("country", help="the country/city to check the weather for")
+args = parser.parse_args()
+
+url = f"{BASE_URL}?q={args.country}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+if response.status_code != 200:
+    print(chalk.red("Error: Unable to retrieve weather information."))
+    exit()
+data = response.json()
+
+temperature = data["main"]["temp"]
+feels_like = data["main"]["feels_like"]
+description = data["weather"][0]["description"]
+icon = data["weather"][0]["icon"]
+city = data["name"]
+country = data["sys"]["country"]
+
+weather_icon = WEATHER_ICONS.get(icon, "")
+output = f"{pyfiglet.figlet_format(city)}, {country}\n\n"
+output += f"{weather_icon} {description}\n"
+output += f"Temperature: {temperature}°C\n"
+output += f"Feels like: {feels_like}°C\n"
+
+# Print output
+print(chalk.green(output))
